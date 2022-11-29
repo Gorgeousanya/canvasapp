7e3a303634ff1b7b1dfe1c372718fe0362619130
gorgeousanya <aplokhotnyuk@rambler.ru>
2022-11-29 16:57:49 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  FC,
+  memo,
+  useReducer,
+  useRef,
+  useEffect,
+} from "react";
+import {
+  createSmartappDebugger,
+  createAssistant,
+  AssistantAppState,
+} from "@sberdevices/assistant-client";
+import "./App.css";
+import { Button, Image } from '@salutejs/plasma-ui';
+import { headline2} from '@salutejs/plasma-tokens';
+import task from './assets/task.png'
+import ListItem from "./components/list-item/ListItem";
+import {
+  isIOS
+} from "react-device-detect";
+import { reducer } from "./store";
 
-function App() {
+const initializeAssistant = (getState: any) => {
+  if (process.env.NODE_ENV === "development") {
+    console.log(getState)
+    return createSmartappDebugger({
+      token: process.env.REACT_APP_TOKEN ?? "",
+      initPhrase: `Запусти ${process.env.REACT_APP_SMARTAPP}`,
+      getState,
+    });
+  }
+
+  return createAssistant({ getState });
+};
+
+export const App: FC = memo(() => {
+  //const [appState, dispatch] = useReducer(reducer, { notes: [] });
+  const list = [
+    {
+      title: "Сервисы Сбера в одном приложении",
+      subtitle: "Информация об инвестициях, страховках и пенсии"
+    },
+    {
+      title: "Вход и оплата в экосистеме по Сбер ID",
+      subtitle: "Не нужно вспоминать пароль и вводить номер карты"
+    },
+    {
+      title: "Персональные предложения и скидки",
+      subtitle: "Возможность получать скидки от партнёров и индивидуальные условия от банка"
+    },
+  ]
+  let link = ""
+  const link_and = "android-app://ru.sberbankmobile/sberbankid/agreement?servicesCode=25?internal_source=audiohelper160503"
+  const link_ios = "sbolonline://sberbankid/omniconsent?servicesCode=25?internal_source=audiohelper160503"
+  const assistantStateRef = useRef<AssistantAppState>();
+  const assistantRef = useRef<ReturnType<typeof createAssistant>>();
+
+  useEffect(() => {
+    assistantRef.current = initializeAssistant(() => assistantStateRef.current);
+    console.log(assistantRef.current)
+    // assistantRef.current.on("data", ({ action }: any) => {
+    //   if (action) {
+    //     dispatch(action);
+    //   }
+    // });
+    link = isIOS ? "sbolonline://sberbankid/omniconsent?servicesCode=25?internal_source=audiohelper160503" :
+    "android-app://ru.sberbankmobile/sberbankid/agreement?servicesCode=25?internal_source=audiohelper160503"
+    console.log(isIOS)
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <Image
+        src={task}
+        width="120px"
+        height="120px"
+        alt="Картинка для примера"
+      />
+      <h2 style={headline2}>
+        Бесплатная услуга
+        <br />
+        "Удобный доступ"
+      </h2>
+      <div style={{ display: "flex", flexDirection: "column", }}>
+        {
+          list.map((item, index) =>
+            <ListItem key={index} index={index + 1} title={item.title} subtitle={item.subtitle} />
+          )
+        }
+        
+        <Button size="m" view="primary" onClick={()=>{console.log(link)}}>
+          <a href={link} android-apk={link_and} > Узнать больше и управлять</a>
+        </Button>
+        {/* <p >
+          После перехода Вы получите дополнительные сведения и расширенную информацию об услуге
+        </p> */}
+      </div>
+    </main>
   );
-}
-
-export default App;
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App } from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { DeviceThemeProvider, SSRProvider } from '@salutejs/plasma-ui';
+import { GlobalStyle } from './components/GlobalStyles';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <DeviceThemeProvider>
+        <SSRProvider>
+            
+            <GlobalStyle />
+            <App />
+        </SSRProvider>
+    </DeviceThemeProvider>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
